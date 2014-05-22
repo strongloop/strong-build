b64f480ef18f426cd5dea4aae2988e78ba0b3d33
Sam Roberts <sam@strongloop.com>
2014-05-22 14:16:58 -0700

    Refactor build test setup so it can be reused

---
+++ test/build-example.js
@@ -0,0 +1,39 @@
+module.exports = function buildExample(args, callback) {
+  assert = require('assert');
+  debug = require('debug')('strong-build:test');
+  var path = require('path');
+
+  require('shelljs/global');
+
+  var build = require('../');
+
+  // Check for node silently exiting with code 0 when tests have not passed.
+  var ok = false;
+
+  process.on('exit', function(code) {
+    if (code === 0) {
+      assert(ok);
+    }
+  });
+
+
+  debug('test: build slc example, args:', args);
+  debug('cwd:', pwd());
+  debug('which slc:', which('slc'), exec('slc -v', {silent: true}).output);
+
+  rm('-rf', '_suite');
+  exec('slc example suite _suite --no-install', {silent: true});
+  cd('_suite');
+  assert(!test('-d', 'node_modules'));
+
+  var argv = ['node', 'slb'].concat(args);
+
+  debug('build with argv:', argv);
+
+  build.build(argv, function(er) {
+    ok = true;
+    return callback(er)
+  });
+
+  debug('waiting for build...');
+};
--- test/test-build.js
+++ test/test-build.js
@@ -1,31 +1,5 @@
-var assert = require('assert');
-var debug = require('debug')('strong-build:test');
-var path = require('path');
-
-require('shelljs/global');
-
-var build = require('../');
-
-// Check for node silently exiting with code 0 when tests have not passed.
-var ok = false;
-
-process.on('exit', function(code) {
-  if (code === 0) {
-    assert(ok);
-  }
-});
-
-
-debug('test:', 'build slc example');
-debug('cwd:', pwd());
-debug('which slc:', which('slc'), exec('slc -v', {silent: true}).output);
-
-rm('-rf', '_suite');
-exec('slc example suite _suite --no-install', {silent: true});
-cd('_suite');
-assert(!test('-d', 'node_modules'));
-
-build.build(process.argv, function(er) {
+// build example with --install
+require('./build-example')(['-i'], function(er) {
   debug('built with error?', er);
   assert.ifError(er);
   assert(test('-d', 'node_modules'));
@@ -40,8 +14,4 @@ build.build(process.argv, function(er) {
   debug('addons:', addons);
 
   assert.equal(addons.length, 0);
-
-  ok = true;
 });
-
-debug('waiting for build...');
