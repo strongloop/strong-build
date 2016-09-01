a9c6d655cd39d181ad4b037fd78ccd30adda62f7
Sam Roberts <vieuxtech@gmail.com>
2016-09-01 13:23:41 -0700

    package: update eslint to 2.x

--- bin/sl-build.js
+++ bin/sl-build.js
@@ -4,6 +4,8 @@
 // This file is licensed under the Artistic License 2.0.
 // License text available at https://opensource.org/licenses/Artistic-2.0
 
+'use strict';
+
 require('../').build(process.argv, function(er) {
   if (!er) {
     process.exit(0);
